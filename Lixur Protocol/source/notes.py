3f7ed6c5a051cc376ad2c40dcdfd23087a50fe15
Naisukhy <87288707+naisukhy@users.noreply.github.com>
2022-04-28 18:01:02 -0400

    Delete Lixur Protocol directory

--- Lixur Protocol/source/notes.py
+++
@@ -1,6 +0,0 @@
-"""
-Estimated Testnet Beta Release: Spring 2022 (v0.1.0)
----------------------------------------------------------------------------------------------------------------------------------------------------------
-1. Solve the potential dilemma of people sending fake graphs using consensus (only an issue if not all online nodes participate in consensus).
-2. Assessing who is online and who is offline and updating the list accordingly.
-"""
--- Lixur Protocol/source/shard.py
+++
@@ -1,6 +0,0 @@
-with open("peers.json", 'r') as f:
-    data = eval(f.read())
-    for x in data:
-        ip = x[0]
-        port = x[1]
-    print(ip, port)
